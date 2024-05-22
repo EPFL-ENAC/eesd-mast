fc492aeed0ed9718cd3c8f425ee52971c91dc520
ymarcon <yannick.marcon@epfl.ch>
2024-05-22 15:10:18 +0200

    feat: redesigned building summary dialog and test page #19

--- frontend/src/i18n/en/index.ts
+++ frontend/src/i18n/en/index.ts
@@ -115,7 +115,7 @@ export default {
   nominal_pga_z: 'Nominal PGA Z-dir. [g]',
   open_measured_data: 'Open Measured Data',
   original_size: 'Original',
-  other_experiments_of_reference: 'Other experiments of this reference',
+  other_experiments_of_reference: 'Other buildings from reference',
   overview: 'Overview',
   paper: 'Paper',
   pga_axis: 'PGA [g units]',
